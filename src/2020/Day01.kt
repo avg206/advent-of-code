58d76e9ba49ca8513cf534fb4c9e7cd055f0e2ed
Anatoli Huseu <avg.tolik@gmail.com>
2023-07-09 15:40:21 +0200

    feat(2020): solve Day 01

---
+++ src/2020/Day01.kt
@@ -0,0 +1,39 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val numbers = input.map { it.toInt() }
+
+        for (i in numbers.indices) {
+            for (j in i + 1 until numbers.size) {
+                if (numbers[i] + numbers[j] == 2020) {
+                    return numbers[i] * numbers[j]
+                }
+            }
+        }
+
+        throw Exception("Not found")
+    }
+
+    fun part2(input: List<String>): Int {
+        val numbers = input.map { it.toInt() }
+
+        for (i in numbers.indices) {
+            for (j in i + 1 until numbers.size) {
+                for (f in j + 1 until numbers.size) {
+                    if (numbers[i] + numbers[j] + numbers[f] == 2020) {
+                        return numbers[i] * numbers[j] * numbers[f]
+                    }
+                }
+            }
+        }
+
+        throw Exception("Not found")
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("2020/Day01_test")
+    check(part1(testInput) == 514579)
+
+    val input = readInput("2020/Day01")
+    part1(input).println()
+    part2(input).println()
+}
