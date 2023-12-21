349ce9da20d22e812367e378cc0920d9e303a195
Anatoli Huseu <avg.tolik@gmail.com>
2023-12-21 08:50:25 +0100

    feat: add neighbours calculation for Point

--- src/helpers/point/Point.kt
+++ src/helpers/point/Point.kt
@@ -8,6 +8,13 @@ data class Point(val x: Int, val y: Int) {
     operator fun times(times: Int): Point {
         return Point(x * times, y * times)
     }
+
+    fun neighbours() = listOf(
+        Point(x + 1, y),
+        Point(x - 1, y),
+        Point(x, y + 1),
+        Point(x, y - 1)
+    )
 }
 
 data class LongPoint(val x: Long, val y: Long) {
@@ -21,8 +28,13 @@ data class LongPoint(val x: Long, val y: Long) {
 }
 
 
-//operator fun <E> List<List<E>>.get(point: Point) = this[point.y][point.x]
+//operator fun <E> List<List<E>>.get(point: Point) = this[point.x][point.y]
+//
 //operator fun <E> List<MutableList<E>>.set(point: Point, value: E) {
-//    this[point.y][point.x] = value
+//    this[point.x][point.y] = value
+//}
+//
+//operator fun <E> MutableList<MutableList<E>>.set(point: Point, value: E) {
+//    this[point.x][point.y] = value
 //}
 
