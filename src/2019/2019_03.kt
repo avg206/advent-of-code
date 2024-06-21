8edfb99ae1959ea240ef792a22532a7b77129bf3
Anatoli Huseu <anatolihuseu@gmail.com>
2024-06-21 22:18:33 +0200

    feat(2019): solve Day 03

---
+++ src/2019/2019_03.kt
@@ -0,0 +1,131 @@
+import kotlin.math.max
+import kotlin.math.min
+import helpers.point.Point
+
+fun main() {
+  fun generateWire(moves: String): List<Point> {
+    val lines = mutableListOf(Point(0, 0))
+
+    for (move in moves.split(",")) {
+      val direction = move[0]
+
+      val distance = move.substring(1).toInt()
+
+      when (direction) {
+        'U' -> lines.add(lines.last() + Point(0, distance))
+        'D' -> lines.add(lines.last() + Point(0, -distance))
+        'L' -> lines.add(lines.last() + Point(-distance, 0))
+        'R' -> lines.add(lines.last() + Point(distance, 0))
+      }
+    }
+
+    return lines
+  }
+
+  fun checkCross(one: List<Point>, two: List<Point>): Point? {
+    when {
+      one[0].x == one[1].x -> {
+        return when {
+          two[0].x == two[1].x -> {
+            null
+          }
+
+          (two[0].y in min(one[0].y, one[1].y)..max(one[0].y, one[1].y))
+            && (one[0].x in min(two[0].x, two[1].x)..max(two[0].x, two[1].x)) -> {
+            Point(one[0].x, two[0].y)
+          }
+
+          else -> null
+        }
+      }
+
+      one[0].y == one[1].y -> {
+        return when {
+          two[0].y == two[1].y -> {
+            null
+          }
+
+          (two[0].x in min(one[0].x, one[1].x)..max(one[0].x, one[1].x))
+            && (one[0].y in min(two[0].y, two[1].y)..max(two[0].y, two[1].y)) -> {
+            Point(two[0].x, one[0].y)
+          }
+
+          else -> null
+        }
+      }
+    }
+
+    return null
+  }
+
+  fun part1(input: List<String>): Int {
+    val wireOne = generateWire(input[0])
+    val wireTwo = generateWire(input[1])
+
+    val start = Point(0, 0)
+
+    var closest: Point? = null
+
+    for (one in wireOne.windowed(2, 1, false)) {
+      for (two in wireTwo.windowed(2, 1, false)) {
+        val point = checkCross(one, two)
+
+        if (point != null && point != start) {
+          if (closest == null || point.manhattan(start) < closest.manhattan(start)) {
+            closest = point
+          }
+        }
+      }
+    }
+
+    return closest!!.manhattan(start)
+  }
+
+  fun part2(input: List<String>): Int {
+    val wireOne = generateWire(input[0])
+    val wireTwo = generateWire(input[1])
+
+    var minDistance = Int.MAX_VALUE
+
+    var oneDistance = 0
+    var twoDistance = 0
+
+    for (one in wireOne.windowed(2, 1, false)) {
+      twoDistance = 0
+
+      for (two in wireTwo.windowed(2, 1, false)) {
+        val point = checkCross(one, two)
+
+        if (point != null && point != Point(0, 0)) {
+          val distance = oneDistance + twoDistance + one[0].manhattan(point) + two[0].manhattan(point)
+
+          if (distance < minDistance) {
+            minDistance = distance
+          }
+        }
+
+        twoDistance += two[0].manhattan(two[1])
+      }
+
+      oneDistance += one[0].manhattan(one[1])
+    }
+
+    return minDistance
+  }
+
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("2019/2019_03_test")
+  val testInput2 = readInput("2019/2019_03_test_2")
+  val testInput3 = readInput("2019/2019_03_test_3")
+  check(part1(testInput) == 6)
+  check(part2(testInput) == 30)
+  check(part2(testInput2) == 610)
+  check(part2(testInput3) == 410)
+
+  val input = readInput("2019/2019_03")
+  check(part1(input) == 403)
+  check(part2(input) == 4158)
+
+  part1(input).println()
+  part2(input).println()
+}
--- src/helpers/point/Point.kt
+++ src/helpers/point/Point.kt
@@ -1,34 +1,40 @@
 package helpers.point
 
+import kotlin.math.abs
+
 data class Point(val x: Int, val y: Int) {
-    operator fun plus(point: Point): Point {
-        return Point(x + point.x, y + point.y)
-    }
+  operator fun plus(point: Point): Point {
+    return Point(x + point.x, y + point.y)
+  }
 
-    operator fun times(times: Int): Point {
-        return Point(x * times, y * times)
-    }
+  operator fun times(times: Int): Point {
+    return Point(x * times, y * times)
+  }
 
-    fun neighbours() = listOf(
-        Point(x + 1, y),
-        Point(x - 1, y),
-        Point(x, y + 1),
-        Point(x, y - 1)
-    )
+  fun neighbours() = listOf(
+    Point(x + 1, y),
+    Point(x - 1, y),
+    Point(x, y + 1),
+    Point(x, y - 1)
+  )
 
-    fun within2DArray(array: List<List<*>>) = x in array.indices && y in array[0].indices
+  fun within2DArray(array: List<List<*>>) = x in array.indices && y in array[0].indices
 
-    fun within2DArrayString(array: List<String>) = x in array.indices && y in array[0].indices
+  fun within2DArrayString(array: List<String>) = x in array.indices && y in array[0].indices
+
+  fun manhattan(start: Point): Int {
+    return abs(x - start.x) + abs(y - start.y)
+  }
 }
 
 data class LongPoint(val x: Long, val y: Long) {
-    operator fun plus(point: LongPoint): LongPoint {
-        return LongPoint(x + point.x, y + point.y)
-    }
+  operator fun plus(point: LongPoint): LongPoint {
+    return LongPoint(x + point.x, y + point.y)
+  }
 
-    operator fun times(times: Long): LongPoint {
-        return LongPoint(x * times, y * times)
-    }
+  operator fun times(times: Long): LongPoint {
+    return LongPoint(x * times, y * times)
+  }
 }
 
 
@@ -37,6 +43,6 @@ operator fun <E> List<List<E>>.get(point: Point) = this[point.x][point.y]
 operator fun List<String>.get(point: Point) = this[point.x][point.y]
 
 operator fun <E> List<MutableList<E>>.set(point: Point, value: E) {
-    this[point.x][point.y] = value
+  this[point.x][point.y] = value
 }
 
